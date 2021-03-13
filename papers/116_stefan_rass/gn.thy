825334cb345725abaec54cd1c948d498e992f61d
Max-Julian Jakobitsch <max-julian.jakobitsch@aau.at>
2021-03-13 18:03:58 +0100

    show correctness of inverse
    apply auto-indent
    add metis specifiers to avoid warnings, reduce and solidify piped facts

--- papers/116_stefan_rass/gn.thy
+++ papers/116_stefan_rass/gn.thy
@@ -5,33 +5,37 @@ begin
 (* actually we could just use num instead of "bool list" since both
    types are constructed exactly the same way as shown below *)
 fun num_of_word :: "bool list \<Rightarrow> num" where
-"num_of_word Nil = num.One" |
-"num_of_word (True#t) = num.Bit1 (num_of_word t)" |
-"num_of_word (False#t) = num.Bit0 (num_of_word t)"
+  "num_of_word Nil = num.One" |
+  "num_of_word (True#t) = num.Bit1 (num_of_word t)" |
+  "num_of_word (False#t) = num.Bit0 (num_of_word t)"
 
 fun word_of_num :: "num \<Rightarrow> bool list" where
-"word_of_num num.One = Nil" |
-"word_of_num (num.Bit1 t) = True#(word_of_num t)" |
-"word_of_num (num.Bit0 t) = False#(word_of_num t)"
+  "word_of_num num.One = Nil" |
+  "word_of_num (num.Bit1 t) = True#(word_of_num t)" |
+  "word_of_num (num.Bit0 t) = False#(word_of_num t)"
 
-lemma word_num_id [simp]: "word_of_num (num_of_word x) = x"
+lemma num_word_num_id [simp]: "word_of_num (num_of_word x) = x"
   apply (induction x)
-  apply (simp)
-  apply (metis num_of_word.simps num_of_word.simps word_of_num.simps(2) word_of_num.simps(3))
+   apply (simp)
+  apply (metis (full_types) num_of_word.simps(2-3) word_of_num.simps(2-3))
   done
 
+lemma word_num_word_id [simp]: "num_of_word (word_of_num x) = x"
+  by (induction x) auto
+
+(* definition of gödel number *)
 fun gn :: "bool list \<Rightarrow> nat" where
   "gn w = nat_of_num (num_of_word w)"
 
 theorem gn_inj: "gn x = gn y \<Longrightarrow> x = y"
 proof -
   assume "gn x = gn y"
-  hence "num_of_word x = num_of_word y" using num_eq_iff by auto
+  hence "num_of_word x = num_of_word y" using num_eq_iff by simp
   then have "word_of_num (num_of_word x) = word_of_num (num_of_word y)" by simp
   from this show ?thesis by simp
 qed
 
-theorem gn_gt_0[simp]: "gn w > 0"
+lemma gn_gt_0[simp]: "gn w > 0"
   by (simp add: nat_of_num_pos)
 
 (* is a number a gödel number *)
@@ -42,8 +46,12 @@ fun is_gn :: "nat \<Rightarrow> bool"
 fun gn_inv :: "nat \<Rightarrow> bool list"
   where "gn_inv n = word_of_num (num_of_nat n)"
 
-lemma "is_gn n \<Longrightarrow> gn (gn_inv n) = n"
-  oops
+(* correctness of the inverse *)
+theorem inv_gn_id [simp]: "is_gn n \<Longrightarrow> gn (gn_inv n) = n"
+  by (induction n) (simp_all add: nat_of_num_inc)
 
-theorem "is_gn n \<Longrightarrow> \<exists>w. gn w = n"
-  oops
+corollary ex_gn: "is_gn n \<Longrightarrow> \<exists>w. gn w = n"
+  using inv_gn_id by blast
+
+theorem gn_inv_id [simp]: "gn_inv (gn (x)) = x"
+  by (simp add: nat_of_num_inverse)
