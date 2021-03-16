4de7b842f0c17a4de0df8ff008020652fa4599b2
Max-Julian Jakobitsch <max-julian.jakobitsch@aau.at>
2021-03-16 12:22:54 +0100

    remove assumption x>0 from lemma4_1
    as it is not actually required

--- papers/116_stefan_rass/dens.thy
+++ papers/116_stefan_rass/dens.thy
@@ -12,28 +12,20 @@ fun dens :: "lang \<Rightarrow> nat \<Rightarrow> nat" where
 fun dens' :: "lang \<Rightarrow> word \<Rightarrow> nat" where
   "dens' L v = dens L (gn v)"
 
-lemma lemma4_1: "\<forall>x>0. dens L x \<le> x"
+lemma lemma4_1: "dens L x \<le> x"
 proof (rule ccontr)
-  assume "\<not> (\<forall>x>0. dens L x \<le> x)"
-  then have "\<exists>x>0. dens L x > x" by (fold not_le) blast
-  then obtain x0 where "x0 < dens L x0" by blast
+  assume "\<not> dens L x \<le> x"
+  then have "dens L x > x" by (fold not_le) blast
 
-  then have "Suc x0 \<le> card {w\<in>L. gn w \<le> x0}" by simp
-  then obtain W where "W \<subseteq> {w\<in>L. gn w \<le> x0}" and Wcard: "card W = Suc x0"
+  then have "Suc x \<le> card {w\<in>L. gn w \<le> x}" by simp
+  then obtain W where "W \<subseteq> {w\<in>L. gn w \<le> x}" and Wcard: "card W = Suc x"
     by (rule obtain_subset_with_card_n)
 
-  then have "gn ` W \<subseteq> {0<..x0}" using gn_gt_0 by auto
-  then have "card (gn ` W) \<le> x0" using card_mono card_greaterThanAtMost
+  then have "gn ` W \<subseteq> {0<..x}" using gn_gt_0 by auto
+  then have "card (gn ` W) \<le> x" using card_mono card_greaterThanAtMost
     by (metis diff_zero finite_greaterThanAtMost)
   then show False using Wcard pigeonhole
     by (metis gn_inj inj_on_def le_imp_less_Suc)
 qed
-
-lemma lemma4_1':
-  fixes L x
-  assumes "x > 0"
-  shows "dens L x \<le> x"
-  using assms and lemma4_1 
-  by blast
 
 end
