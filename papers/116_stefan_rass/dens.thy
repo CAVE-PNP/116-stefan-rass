756dcebf99b9920860728255dff1047fecb283c2
Max-Julian Jakobitsch <max-julian.jakobitsch@aau.at>
2021-03-17 14:39:40 +0100

    proof of 4.1 / eq6
    using preimage of gn
    refactor existing proofs

--- papers/116_stefan_rass/dens.thy
+++ papers/116_stefan_rass/dens.thy
@@ -12,6 +12,20 @@ fun dens :: "lang \<Rightarrow> nat \<Rightarrow> nat" where
 fun dens' :: "lang \<Rightarrow> word \<Rightarrow> nat" where
   "dens' L v = dens L (gn v)"
 
+lemma vim_le:
+  fixes x :: nat
+  shows "{w. f w \<le> x} = f -` {0..x}"
+  by fastforce
+
+lemma vim_le2:
+  fixes x :: nat
+  shows "{w \<in> A. f w \<le> x} = A \<inter> f -` {0..x}"
+  using vim_le[of f x] by blast
+
+(* definition of dens is equivalent to using preimage of gn intersected with L *)
+lemma dens_eq_card_vim_gn: "dens L x = card (L \<inter> gn -` {0..x})"
+  using dens.simps[of L x] unfolding vim_le2[of L gn x] .
+
 lemma lemma4_1: "dens L x \<le> x"
 proof (rule ccontr)
   assume "\<not> dens L x \<le> x"
@@ -27,11 +41,21 @@ proof (rule ccontr)
     by (metis gn_inj inj_on_def le_imp_less_Suc)
 qed
 
-lemma bounded_lang_finite : "finite {w\<in>L. gn w \<le> x}" by sorry
+lemma set_un_le: (* currently unused *)
+  fixes x :: nat and P :: "'a \<Rightarrow> bool" and f :: "'a \<Rightarrow> nat"
+  shows set_un_le_lt_eq: "{w. P w \<and> f w \<le> x} = {w. P w \<and> f w < x} \<union> {w. P w \<and> f w = x}"
+    and set_un_le_Suc: "{w. P w \<and> f w \<le> (Suc x)} = {w. P w \<and> f w \<le> x} \<union> {w. P w \<and> f w = (Suc x)}"
+  by auto
+
+lemma bounded_lang_finite: "finite {w \<in> L. gn w \<le> x}"
+proof -
+  from gn_inj2 have "finite (gn -` {0..x})" using finite_vimageI[of "{0..x}" gn] by blast
+  then have "finite (L \<inter> (gn -` {0..x}))" by blast
+  then show "finite {w \<in> L. gn w \<le> x}" by (fold vim_le2[of L gn x])
+qed
 
 lemma dens_mono: "L \<subseteq> M \<Longrightarrow> dens L x \<le> dens M x"
 proof -
-  fix x
   assume "L \<subseteq> M"
   hence "{w \<in> L. gn w \<le> x} \<subseteq> {w \<in> M. gn w \<le> x}" by blast
   thus "dens L x \<le> dens M x"
@@ -39,8 +63,6 @@ proof -
 qed
 
 lemma dens_intersect_le: "dens (L1 \<inter> L2) x \<le> dens L1 x"
-proof -
-  from dens_mono show ?thesis by blast
-qed
+  using dens_mono by blast
 
 end
