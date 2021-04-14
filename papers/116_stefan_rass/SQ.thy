8c10796c62b09766bd2f0662a604be004a4becca
Max-Julian Jakobitsch <max-julian.jakobitsch@aau.at>
2021-04-14 15:08:08 +0200

    plug holes and tune proofs

--- papers/116_stefan_rass/SQ.thy
+++ papers/116_stefan_rass/SQ.thy
@@ -13,7 +13,7 @@ definition SQ_nat :: "nat set" where \<comment> \<open>the analogous set \<open>
 
 declare SQ_def[simp] SQ_nat_def[simp]
 
-lemma SQ_nat_zero: 
+lemma SQ_nat_zero:
 	"insert 0 SQ_nat = {y. \<exists>x. y = x ^ 2}"
 	"SQ_nat = {y. \<exists>x. y = x ^ 2} - {0}"
 	by auto
@@ -49,7 +49,7 @@ proof -
   have eq: "{w\<in>SQ. gn w \<le> x} = gn_inv ` power2 ` {0<..Discrete.sqrt x}"
   proof -
     have "{w\<in>SQ. gn w \<le> x} = {w. \<exists>z. (gn w = z^2 \<and> gn w \<le> x)}"
-      using SQ_def by (smt (verit, best) Collect_cong mem_Collect_eq)
+      using SQ_def by simp
     also have "\<dots> = {w. \<exists>z. (gn w = z^2 \<and> z^2 \<le> x)}" by metis
     also have "\<dots> = {w. \<exists>z. (gn w = z^2 \<and> 0 < z^2 \<and> z^2 \<le> x)}"
       using gn_def nat_of_num_pos by metis
@@ -61,7 +61,8 @@ proof -
     also have "\<dots> = gn_inv ` power2 ` {0<..Discrete.sqrt x}" by auto
     finally show ?thesis .
   qed
-  then have "inj_on gn_inv (power2 ` {0<..Discrete.sqrt x})"
+
+  have "inj_on gn_inv (power2 ` {0<..Discrete.sqrt x})"
     by (smt (verit, best) gn_inv_def greaterThanAtMost_iff imageE inj_on_def nat_zero_less_power_iff num_of_nat_inverse)
   then have "dens SQ x = card (power2 ` {0<..Discrete.sqrt x})"
     using eq card_image by auto
@@ -111,7 +112,7 @@ next
   case True then show ?thesis
   proof (cases "is_square n")
     case True
-    with next_sq_eq[of n] and \<open>n > 0\<close> show ?thesis using eq_imp_le by presburger 
+    with next_sq_eq[of n] and \<open>n > 0\<close> show ?thesis using eq_imp_le by presburger
   next
     case False
     with next_sq_gt[of n] and \<open>n > 0\<close> show ?thesis using less_imp_le_nat by blast
@@ -151,41 +152,78 @@ proof -
   then show "?b - ?a \<le> 2 * ?s + 1" unfolding adj_sq_nat .
 qed
 
+
+subsection\<open>Log inequality\<close>
+
+lemma nat_pow_nat:
+  fixes b :: nat and x :: int
+  assumes "x \<ge> 0" "b > 0"
+  shows "(b powr x) \<in> \<nat>"
+proof -
+  from assms have "b powr x = b ^ (nat x)" using powr_real_of_int by simp
+  moreover have "real (b ^ (nat x)) \<in> \<nat>" using of_nat_in_Nats by blast
+  ultimately show ?thesis by simp
+qed
+
+lemma nat_eq_ceil_floor:
+  fixes n :: real
+  assumes "n \<in> \<nat>"
+  shows nat_eq_ceil: "\<lceil>n\<rceil> = n"
+    and nat_eq_floor: "\<lfloor>n\<rfloor> = n"
+  using assms
+  by (cases n rule: Nats_cases; simp)+
+
 lemma log_ceil_le:
-  assumes "x > 0"
+  assumes "x \<ge> 1"
   shows "log 2 \<lceil>x\<rceil> \<le> \<lceil>log 2 x\<rceil>"
 proof -
-  have "log 2 x \<le> \<lceil>log 2 x\<rceil>" using le_of_int_ceiling .
-  then have "2 powr (log 2 x) \<le> 2 powr \<lceil>log 2 x\<rceil>" by simp
-  then have "x \<le> 2 powr \<lceil>log 2 x\<rceil>"
-    by (smt (verit, ccfv_threshold) Transcendental.log_le_iff powr_gt_zero)
-  then have "\<lceil>x\<rceil> \<le> \<lceil>2 powr \<lceil>log 2 x\<rceil>\<rceil>" by (rule ceiling_mono)
-  then have "\<lceil>x\<rceil> \<le> 2 powr \<lceil>log 2 x\<rceil>" sorry (* 2 powr \<lceil>log 2 x\<rceil> \<in> nat *)
-  thus "log 2 \<lceil>x\<rceil> \<le> \<lceil>log 2 x\<rceil>"
-    using Transcendental.log_le_iff assms by simp
+  from \<open>x \<ge> 1\<close> have "x = 2 powr (log 2 x)" using powr_log_cancel[of 2 x] by simp
+  also have "2 powr (log 2 x) \<le> 2 powr \<lceil>log 2 x\<rceil>" by simp
+  finally have "x \<le> 2 powr \<lceil>log 2 x\<rceil>" .
+
+  then have *: "\<lceil>x\<rceil> \<le> \<lceil>2 powr \<lceil>log 2 x\<rceil>\<rceil>" by (rule ceiling_mono)
+  have "\<lceil>x\<rceil> \<le> 2 powr \<lceil>log 2 x\<rceil>"
+  proof -
+    from \<open>x \<ge> 1\<close> have "log 2 x \<ge> 0" by simp
+    then have "\<lceil>log 2 x\<rceil> \<ge> 0" by simp
+    then have "2 powr \<lceil>log 2 x\<rceil> \<in> \<nat>" using nat_pow_nat[of "\<lceil>log 2 x\<rceil>" 2] by simp
+    then have "\<lceil>2 powr \<lceil>log 2 x\<rceil>\<rceil> = 2 powr \<lceil>log 2 x\<rceil>" using nat_eq_ceil by simp
+    with * show ?thesis by auto
+  qed
+  thus "log 2 \<lceil>x\<rceil> \<le> \<lceil>log 2 x\<rceil>" using Transcendental.log_le_iff assms by simp
 qed
+
+lemma log2_sqrt:
+  fixes x :: real
+  assumes "x > 0"
+  shows "log 2 (sqrt x) = (log 2 x) / real 2"
+  unfolding sqrt_def
+  using log_root[of 2 x 2] and pos2 \<open>x > 0\<close> .
+
+corollary log2_sqrt':
+  fixes x :: real
+  assumes "x > 0"
+  shows "log 2 (sqrt x) = (log 2 x) / 2"
+  using log2_sqrt and \<open>x > 0\<close> by simp
 
 lemma delta_bitlength:
   assumes "1 \<le> x"
   shows "\<lceil>log 2 (2*\<lceil>sqrt x\<rceil>+1)\<rceil> \<le> 3 + \<lceil>log 2 x\<rceil> / 2"
 proof -
   have le1: "\<lceil>x/2\<rceil> \<le> 1 + \<lceil>x\<rceil>/2" by linarith
-  have le2: "1 \<le> \<lceil>sqrt x\<rceil>" using assms by simp
-  have le3: "(log 2 3) \<le> 2" sorry
-  with le2 have "\<lceil>log 2 (2*\<lceil>sqrt x\<rceil>+1)\<rceil> \<le> \<lceil>log 2 (3*\<lceil>sqrt x\<rceil>)\<rceil>"
+  have le2: "1 \<le> \<lceil>sqrt x\<rceil>" using \<open>1 \<le> x\<close> by simp
+  have le3: "log 2 3 < real 2" using less_powr_iff[of 2 3 2] by force
+  have le4: "log 2 (sqrt x) = (log 2 x) / 2" using log2_sqrt'[of x] and \<open>1 \<le> x\<close> by simp
+
+  from le2 have "\<lceil>log 2 (2*\<lceil>sqrt x\<rceil>+1)\<rceil> \<le> \<lceil>log 2 (3*\<lceil>sqrt x\<rceil>)\<rceil>"
     by (smt (verit, del_insts) ceiling_mono log_le_cancel_iff of_int_le_1_iff of_int_le_iff)
-  also have "\<dots> = \<lceil>(log 2 3) + log 2 \<lceil>sqrt x\<rceil>\<rceil>"
-    using log_mult [of 2 3] sorry
-  also have "\<dots> \<le> \<lceil>2 + log 2 \<lceil>sqrt x\<rceil>\<rceil>"
-    using le3 ceiling_mono by (simp add: ceiling_mono)
+  also have "\<dots> = \<lceil>(log 2 3) + log 2 \<lceil>sqrt x\<rceil>\<rceil>" using le2 and log_mult[of 2 3 "\<lceil>sqrt x\<rceil>"] by auto
+  also have "\<dots> \<le> \<lceil>2 + log 2 \<lceil>sqrt x\<rceil>\<rceil>" using le3 ceiling_mono by (simp add: ceiling_mono)
   also have "\<dots> = 2 + \<lceil>log 2 \<lceil>sqrt x\<rceil>\<rceil>" by linarith
-  also have "\<dots> \<le> 2 + \<lceil>\<lceil>log 2 (sqrt x)\<rceil>\<rceil>"
-    using log_ceil_le by (smt (verit, ccfv_SIG) ceiling_mono le2 zero_less_ceiling)
+  also have "\<dots> \<le> 2 + \<lceil>\<lceil>log 2 (sqrt x)\<rceil>\<rceil>" using log_ceil_le by (simp add: assms ceiling_le_iff)
   also have "\<dots> = 2 + \<lceil>log 2 (sqrt x)\<rceil>" by simp
-  also have "\<dots> = 2 + \<lceil>(log 2 x)/2\<rceil>"
-    by (smt (verit, ccfv_SIG) le2 log_root numeral_plus_numeral of_nat_numeral one_plus_numeral pos2 real_sqrt_gt_0_iff semiring_norm(2) sqrt_def zero_less_ceiling)
-  also have "\<dots> \<le> 3 + \<lceil>log 2 x\<rceil> / 2"
-    using le1  by linarith
+  also have "\<dots> = 2 + \<lceil>(log 2 x) / 2\<rceil>" unfolding le4 by (rule refl)
+  also have "\<dots> \<le> 3 + \<lceil>log 2 x\<rceil> / 2" using le1 by linarith
   finally show ?thesis by simp
 qed
 
