2805bdbb19924473de15a3f495b6cebe4c1007c5
Stefan Haan <stefan.haan@aau.at>
2021-07-10 10:20:54 +0200

    moved defintions in pre_digraph

--- papers/049_zohreh_akbari/Clique.thy
+++ papers/049_zohreh_akbari/Clique.thy
@@ -12,29 +12,31 @@ maximum clique
 largest subgraph of G in which alpha exists
 *)
 
-definition clique :: "('a, 'b) pre_digraph \<Rightarrow> 'a set \<Rightarrow> bool"
-  where "clique G C = complete_digraph (card C) (G \<restriction> C)"
+definition (in pre_digraph) clique :: "'a set \<Rightarrow> bool"
+  where "clique C = complete_digraph (card C) (G \<restriction> C)"
 
-definition max_clique :: "('a, 'b) pre_digraph \<Rightarrow> 'a set"
-  where "max_clique G = (ARG_MAX card S. (clique G S))"
+definition (in pre_digraph) max_clique :: "'a set"
+  where "max_clique = (ARG_MAX card S. clique S)"
 
-definition least_degree_vertex :: "('a, 'b) pre_digraph \<Rightarrow> 'a"
-  where "least_degree_vertex G = (ARG_MIN (\<lambda>\<alpha>. out_degree G \<alpha>) \<alpha>. (\<alpha> \<in> verts G))"
+definition (in pre_digraph) least_degree_vertex :: "'a"
+  where "least_degree_vertex = (ARG_MIN (\<lambda>\<alpha>. out_degree G \<alpha>) \<alpha>. \<alpha> \<in> verts G)"
 
-definition neighborhood :: "('a, 'b) pre_digraph \<Rightarrow> 'a \<Rightarrow> ('a, 'b) pre_digraph"
-  where "neighborhood G \<alpha> = G \<restriction> {v. v = \<alpha> \<or> \<alpha> \<rightarrow>\<^bsub>G\<^esub> v \<or> v \<rightarrow>\<^bsub>G\<^esub> \<alpha>}"
+definition (in pre_digraph) neighbors :: "'a \<Rightarrow> 'a set"
+  where "neighbors \<alpha> = {v. v = \<alpha> \<or> \<alpha> \<rightarrow>\<^bsub>G\<^esub> v \<or> v \<rightarrow>\<^bsub>G\<^esub> \<alpha>}"
+
+definition (in pre_digraph) neighborhood :: "'a \<Rightarrow> ('a, 'b) pre_digraph"
+  where "neighborhood \<alpha> = G \<restriction> neighbors \<alpha>"
 
 
 lemma (in fin_digraph) least_degree_vertex_ex:
   assumes "verts G \<noteq> {}"
-  shows "least_degree_vertex G \<in> verts G"
+  shows "least_degree_vertex \<in> verts G"
   using arg_min_if_finite(1) finite_verts assms unfolding least_degree_vertex_def arg_min_on_def .
 
-thm fin_digraph.least_degree_vertex_ex
 
 lemma (in graph) singleton_clique:
   assumes "v \<in> verts G"
-  shows "clique G {v}"
+  shows "clique {v}"
   unfolding clique_def complete_digraph_def
 proof (intro conjI)
   let ?H = "G \<restriction> {v}"
@@ -51,24 +53,28 @@ proof (intro conjI)
     using * by simp
 qed
 
-lemma neighbor_selfI: "\<alpha> \<in> verts (neighborhood G \<alpha>)" unfolding neighborhood_def by simp
-lemma neighbor_inI: "\<alpha> \<rightarrow>\<^bsub>G\<^esub> v \<Longrightarrow> v \<in> verts (neighborhood G \<alpha>)" unfolding neighborhood_def by simp
-lemma neighbor_outI: "v \<rightarrow>\<^bsub>G\<^esub> \<alpha> \<Longrightarrow> v \<in> verts (neighborhood G \<alpha>)" unfolding neighborhood_def by simp 
+lemma (in pre_digraph) neighborI:
+  shows neighbor_selfI: "\<alpha> \<in> verts (neighborhood \<alpha>)"
+    and neighbor_inI: "\<alpha> \<rightarrow>\<^bsub>G\<^esub> v \<Longrightarrow> v \<in> verts (neighborhood \<alpha>)"
+    and neighbor_outI: "v \<rightarrow>\<^bsub>G\<^esub> \<alpha> \<Longrightarrow> v \<in> verts (neighborhood \<alpha>)"
+unfolding neighborhood_def neighbors_def by simp_all
 
-lemma neighborE:
-  assumes "v \<in> verts (neighborhood G \<alpha>)"
+
+lemma (in pre_digraph) neighborE:
+  assumes "v \<in> verts (neighborhood \<alpha>)"
   shows "v = \<alpha> \<or> \<alpha> \<rightarrow>\<^bsub>G\<^esub> v \<or> v \<rightarrow>\<^bsub>G\<^esub> \<alpha>"
-  using assms unfolding neighborhood_def by simp
+  using assms unfolding neighborhood_def neighbors_def by simp
 
-lemma nbh_altdef: "neighborhood G \<alpha> = G \<restriction> ({\<alpha>} \<union> {v. \<alpha> \<rightarrow>\<^bsub>G\<^esub> v} \<union> {v. v \<rightarrow>\<^bsub>G\<^esub> \<alpha>})"
-  unfolding neighborhood_def sorry
-  (* sledgehammer claims solved but solution times out: by (metis Collect_disj_eq Un_insert_left insert_is_Un singleton_conv) *)
+lemma (in pre_digraph) nb_altdef: "neighbors \<alpha> = ({\<alpha>} \<union> {v. \<alpha> \<rightarrow>\<^bsub>G\<^esub> v} \<union> {v. v \<rightarrow>\<^bsub>G\<^esub> \<alpha>})"
+  unfolding neighbors_def by auto
+
+lemma (in pre_digraph) nbh_altdef: "neighborhood \<alpha> = G \<restriction> ({\<alpha>} \<union> {v. \<alpha> \<rightarrow>\<^bsub>G\<^esub> v} \<union> {v. v \<rightarrow>\<^bsub>G\<^esub> \<alpha>})"
+  unfolding neighborhood_def using nb_altdef by simp
 
 
 subsection\<open>Helper lemmas\<close>
 
 lemma pair_inj: "inj (Pair a)" by (intro injI) (rule Pair_inject)
-
 
 lemma inj_on_arc_to_ends [simp]:
   assumes "graph G"
@@ -77,7 +83,7 @@ lemma inj_on_arc_to_ends [simp]:
 
 lemma (in graph) inj_on_arc_to_ends [simp]: (* apparently, defining this using the locale interferes with the [simp] annotation *)
   shows "inj_on (arc_to_ends G) (arcs G)"
-  using local.inj_on_arc_to_ends .
+  using inj_on_arc_to_ends .
 
 lemma inj_imp_inj_on: "inj f \<Longrightarrow> inj_on f A"
   using subset_UNIV by (subst inj_on_subset) blast+
