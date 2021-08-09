9bddde898a28a90853dab27802fe8896c90cad2d
Stefan Haan <stefan.haan@aau.at>
2021-08-09 16:29:17 +0900

    make threshold theorem smell less fishy

--- papers/116_stefan_rass/Threshold_Functions.thy
+++ papers/116_stefan_rass/Threshold_Functions.thy
@@ -1,71 +1,45 @@
 theory Threshold_Functions
-  imports Main "HOL.Binomial" "HOL.Real" "HOL.Limits"
+  imports Main "HOL.Binomial" "HOL.Limits"
 begin
 
-definition th_prop where
-  "th_prop n Q \<equiv> Collect Q \<subseteq> Pow {1..n}"
+definition mono
+  where "mono Q \<equiv> \<forall>A B. A \<subseteq> B \<longrightarrow> Q A \<longrightarrow> Q B"
 
-definition th_mono_inc where
-  "th_mono_inc (n::nat) Q \<equiv> \<forall>B\<subseteq>{1..n}. \<forall>A\<subseteq>B. Q A \<longrightarrow> Q B"
-
-definition th_mono_dec where
-  "th_mono_dec (n::nat) Q \<equiv> (\<forall>A\<subseteq>{1..n}. \<forall>B\<subseteq>A. Q A \<longrightarrow> Q B)"
-
-lemma th_mono_inc_dec : "th_mono_inc n Q \<longleftrightarrow> th_mono_dec n (\<lambda>A. \<not> Q A)"
-  unfolding th_mono_inc_def th_mono_dec_def
-  by (safe) blast+
-
-definition th_non_triv where
-  "th_non_triv (n::nat) Q \<equiv> Collect Q \<noteq> {} \<and> Collect Q \<subset> Pow {1..n}"
-
-lemma th_non_triv_mono:
-  assumes "th_prop n Q"
-      and "th_mono_inc n Q"
-    shows "th_non_triv n Q \<longleftrightarrow> \<not> Q {} \<and> Q {1..n}"
-using assms
-proof (intro iffI conjI)
-  fix n Q assume assms: "th_non_triv n Q" "th_mono_inc n Q"
-
-  show "\<not> Q {}" proof
-    assume "Q {}"
-    then have "\<forall>A\<subseteq>{1..n}. Q A"
-      using assms unfolding th_non_triv_def th_mono_inc_def by blast
-    thus False using assms(1) unfolding th_non_triv_def by blast
-  qed
-  
-  from assms(1) obtain A where "A \<subseteq> {1..n}" and "Q A"
-    unfolding th_non_triv_def by auto
-  then show "Q {1..n}"
-    using assms(2) unfolding th_mono_inc_def by blast
-next
-  fix n::nat and Q assume "\<not> Q {} \<and> Q {1..n}" "th_prop n Q"
-  then show "th_non_triv n Q"
-    unfolding th_non_triv_def th_prop_def by blast
-qed
+definition non_trivial
+  where "non_trivial Q \<equiv> \<exists>A B. Q A \<and> ~ Q B"
 
 (* probability of a random k-subset of {1..n} having Q *)
-definition th_Pk where
-  "th_Pk n k Q \<equiv> card {A\<in>Pow {1..n}. card A = k \<and> Q A} / (binomial n k)"
+definition P_k
+  where "P_k n k Q \<equiv> card {A\<in>Pow {1..n}. card A = k \<and> Q A} / (binomial n k)"
 
-definition th_almost_every where
-  "th_almost_every k Q \<equiv> (\<lambda>n. th_Pk n (k n) Q) \<longlonglongrightarrow> 1"
+corollary mono_non_triv_0:
+  assumes "mono Q" and "non_trivial Q"
+  shows "P_k n 0 Q = 0"
+  oops
 
-definition th_almost_none where
-  "th_almost_none k Q \<equiv> (\<lambda>n. th_Pk n (k n) Q) \<longlonglongrightarrow> 0"
+corollary mono_non_triv_n:
+  assumes "mono Q" and "non_trivial Q"
+  shows "P_k n n Q = 1"
+  oops
 
-definition th_threshold where
-  "th_threshold M Q \<equiv>
+definition almost_every
+  where "almost_every k Q \<equiv> (\<lambda>n. P_k n (k n) Q) \<longlonglongrightarrow> 1"
+
+definition almost_none 
+  where "almost_none k Q \<equiv> (\<lambda>n. P_k n (k n) Q) \<longlonglongrightarrow> 0"
+
+definition threshold
+  where "threshold M Q \<equiv>
     \<forall>m. let q=\<lambda>n. (m n)/of_nat (M n) in
-      (q \<longlonglongrightarrow> 0) \<longrightarrow> th_almost_none m Q
-    \<and> (q \<longlonglongrightarrow> 1) \<longrightarrow> th_almost_every m Q"
+      (q \<longlonglongrightarrow> 0) \<longrightarrow> almost_none m Q
+    \<and> (q \<longlonglongrightarrow> 1) \<longrightarrow> almost_every m Q"
 
 (*every non-trivial monotone increasing property has a threshold function *)
 theorem mono_ex_th:
-  assumes "th_mono_inc n Q"
-      and "th_non_triv n Q"
-    obtains M where "th_threshold M Q"
-  sorry
-
-(* something's fishy because mono_inc and non_triv are defined with n but th_threshold is not *)
+  fixes Q :: "nat set \<Rightarrow> bool"
+  assumes "mono Q"
+      and "non_trivial Q"
+    obtains M where "threshold M Q"
+sorry
 
 end
