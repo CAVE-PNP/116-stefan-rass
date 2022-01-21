894eaa818e7ab9753f807aa232a75213f06c8c8b
Max-Julian Jakobitsch <max-julian.jakobitsch@aau.at>
2022-01-21 13:04:47 +0100

    move DEV session in separate dir, split in current/all
    `DEV_CAVE-PNP` ("current") reflects the current state of the leading branches and expected developments
    `DEV_CAVE-PNP_ALL` ("all") summarizes all imports that have been used, are currently used, or will possibly be used in the future

---
+++ papers/_dev/all/DEV_ALL.thy
@@ -0,0 +1,16 @@
+(* main theory of dummy session "DEV_CAVE-PNP_ALL",
+ * designed to collect all dependencies for easy access *)
+theory DEV_ALL
+  imports
+    (* 049 Clique *)
+    "Graph_Theory.Graph_Theory"
+    "Simpl.Simpl"
+    "HOL-Imperative_HOL.Imperative_HOL"
+    (* 116 OWF *)
+    "HOL-Library.Sublist"
+    "HOL-Library.Countable_Set"
+    "Universal_Turing_Machine.UTM"
+    "HOL-Computational_Algebra.Primes"
+    "Intro_Dest_Elim.IHOL_IDE"
+begin
+end
