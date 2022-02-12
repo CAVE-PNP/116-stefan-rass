CAVE-PNP/116-stefan-rass

commit b2885a2cb12d2a5d843ca48ad3df2f0912a567f3

Proper citations, LaTeX improvements
move referenced THT_inconsistencies_MWE.thy into papers/116_stefan_rass/examples

papers/116_stefan_rass/SQ.thy
@@ -9,13 +9,13 @@ theory SQ
 begin
 
 
-text\<open>SQ is an overloaded identifier in the paper.
+text\<open>SQ is an overloaded identifier in @{cite rassOwf2017}.
   However, the more common notion is the language as opposed to the set of natural numbers.\<close>
 
 definition SQ :: lang \<comment> \<open>The language of non-zero square numbers, represented by binary strings without leading ones.\<close>
   where [simp]: "SQ \<equiv> {w. \<exists>x. gn w = x\<^sup>2}"
 
-definition SQ_nat :: "nat set" \<comment> \<open>The analogous set \<open>SQ \<subseteq> \<nat>\<close>, as defined in ch 4.1\<close>
+definition SQ_nat :: "nat set" \<comment> \<open>The analogous set \<open>SQ \<subseteq> \<nat>\<close>, as defined in @{cite \<open>ch.~4.1\<close> rassOwf2017}.\<close>
   where "SQ_nat \<equiv> {y. y \<noteq> 0 \<and> (\<exists>x. y = x\<^sup>2)}"
 
 lemma SQ_nat_zero:
@@ -23,7 +23,7 @@ lemma SQ_nat_zero:
 	"SQ_nat = {y. \<exists>x. y = x ^ 2} - {0}"
 	by (auto simp add: SQ_nat_def)
 
-\<comment> \<open>relating \<^const>\<open>SQ\<close> and \<^const>\<open>SQ_nat\<close>\<close>
+text\<open>Relating \<^const>\<open>SQ\<close> and \<^const>\<open>SQ_nat\<close>:\<close>
 lemma SQ_SQ_nat:
   shows SQ_nat_vim: "SQ = gn -` SQ_nat"
     and SQ_nat_eq: "SQ = {w. gn w \<in> SQ_nat}"
@@ -50,8 +50,8 @@ next
 qed
 
 
-text\<open>"Lemma 4.2. The language of squares \<open>SQ = {y. y = x\<^sup>2 \<and> x \<in> \<nat>}\<close>
-  has a density function \<open>dens\<^sub>S\<^sub>Q(x) \<in> \<Theta>(\<surd>x)\<close>."\<close>
+text\<open>``Lemma 4.2 @{cite rassOwf2017}. The language of squares \<open>SQ = {y. y = x\<^sup>2 \<and> x \<in> \<nat>}\<close>
+  has a density function \<open>dens\<^sub>S\<^sub>Q(x) \<in> \<Theta>(\<surd>x)\<close>.''\<close>
 
 theorem dens_SQ: "dens SQ x = dsqrt x"
 proof -
@@ -92,7 +92,7 @@ proof -
 qed
 
 
-subsection\<open>Log inequality\<close>
+subsection\<open>Log Inequality\<close>
 
 lemma nat_pow_nat:
   fixes b :: nat and x :: int
@@ -170,9 +170,9 @@ proof (cases "x > 0")
 qed (* case "x \<le> 0" by *) simp
 
 
-subsection\<open>Length of prefix\<close>
+subsection\<open>Length of Prefix\<close>
 
-\<comment> \<open>\<open>w'\<close> (next_square n) will eventually have an identical lot of \<open>\<lceil>log l\<rceil>\<close> most significant bits\<close>
+text\<open>\<open>w'\<close> (next_square n) will eventually have an identical lot of \<open>\<lceil>log l\<rceil>\<close> most significant bits.\<close>
 
 (* TODO format the following note as text *)
 (*

papers/116_stefan_rass/Binary.thy
@@ -7,7 +7,7 @@ theory Binary
 begin
 
 
-text\<open>In the paper "On the Existence of Weak One-Way Functions",
+text\<open>In @{cite rassOwf2017},
   a binary alphabet \<open>\<Sigma> := {0, 1}\<close> is used for virtually all TM related definitions.
   Therefore, a strong theory of binary strings is required for reasoning.
 
@@ -44,16 +44,16 @@ fun bin_of_nat :: "nat \<Rightarrow> bin" where
   "bin_of_nat 0 = []" |
   "bin_of_nat (Suc n) = inc (bin_of_nat n)"
 
-\<comment> \<open>For binary numbers, as stated in the paper (ch. 1, p. 2),
+text\<open>For binary numbers, as stated in @{cite \<open>ch.~1\<close> rassOwf2017},
   the "least significant bit is located at the right end".
-  The recursive definitions for binary strings result in somewhat unintuitive definitions:
+  The recursive definitions for binary strings result in somewhat less intuitive definitions:
   The number \<open>6\<^sub>1\<^sub>0\<close> is written \<open>110\<^sub>2\<close> in binary, but as \<^typ>\<open>bin\<close>,
-  it is \<^term>\<open>[False, True, True]\<close> (an abbreviation for \<^term>\<open>False # True # True # []\<close>).
+  it is \<^term>\<open>[False, True, True]\<close> (an abbreviation for \<open>False # True # True # []\<close>).
   This results in some strange properties including swapping prefix and suffix:
   the concepts of \<^const>\<open>prefix\<close> and \<^const>\<open>suffix\<close> defined over lists (see \<^theory>\<open>HOL-Library.Sublist\<close>)
   mean their exact opposites when applied to our definition of \<^typ>\<open>bin\<close>.\<close>
 
-value "([False, True, True])\<^sub>2"
+value "([False, True, True])\<^sub>2" \<comment> \<open>returns @{value "([False, True, True])\<^sub>2"}\<close>
 
 
 subsection\<open>Numeric Properties\<close>
@@ -183,6 +183,8 @@ text\<open>The number of bits in the binary representation.
 
 abbreviation (input) bit_length :: "nat \<Rightarrow> nat" where
   "bit_length n \<equiv> length (bin_of_nat n)"
+
+value "bit_length 0" \<comment> \<open>is @{value "bit_length 0"}\<close>
 
 
 lemma bit_length_mono: "mono bit_length"
@@ -452,7 +454,7 @@ lemma bit_length_eq_log:
 
 subsection\<open>Order\<close>
 
-text\<open>From ch. 4.4, p14: "we will order two words \<open>u, v \<in> \<Sigma>\<^sup>*\<close> as \<open>u \<le> v \<Longleftrightarrow> (u)\<^sub>2 \<le> (v)\<^sub>2\<close>."
+text\<open>From @{cite \<open>ch.~4.4\<close> rassOwf2017}: "we will order two words \<open>u, v \<in> \<Sigma>\<^sup>*\<close> as \<open>u \<le> v \<Longleftrightarrow> (u)\<^sub>2 \<le> (v)\<^sub>2\<close>."
   Note: defining the \<^const>\<open>less\<close> relation is necessary for \<^class>\<open>ord\<close>
   (of which \<^class>\<open>preorder\<close> is a subclass).
   As anti-symmetry is not given, no partial order (\<^class>\<open>order\<close>) can be established.\<close>

papers/116_stefan_rass/Complexity.thy
@@ -89,19 +89,19 @@ subsubsection\<open>Time\<close>
 
 text\<open>The time restriction predicate is similar to \<^term>\<open>Hoare_halt\<close>,
   but includes a maximum number of steps.
-  From Hopcroft, p287f:
-   "If for every input word of length n, M makes at most T(n) moves before
+  From @{cite \<open>ch.~12.1\<close> hopcroftAutomata1979}:
+  ``If for every input word of length n, M makes at most T(n) moves before
     halting, then M is said to be a T(n) time-bounded Turing machine, or of time
-    complexity T(n). The language recognized by M is said to be of time complexity T(n)."
+    complexity T(n). The language recognized by M is said to be of time complexity T(n).''
 
-   "[...] it is reasonable to assume that any time complexity function \<open>T(n)\<close> is
+  ``[...] it is reasonable to assume that any time complexity function \<open>T(n)\<close> is
     at least \<open>n + 1\<close>, for this is the time needed just to read the input and verify that the
     end has been reached by reading the first blank.* We thus make the convention
-    that "time complexity \<open>T(n)\<close>" means \<open>max (n + 1, \<lceil>T(n)\<rceil>])\<close>. For example, the value of
+    that `time complexity \<open>T(n)\<close>' means \<open>max (n + 1, \<lceil>T(n)\<rceil>])\<close>. For example, the value of
     time complexity \<open>n log\<^sub>2n\<close> at \<open>m = 1\<close> is \<open>2\<close>, not \<open>0\<close>, and at \<open>n = 2\<close>, its value is \<open>3\<close>.
 
     * Note, however, that there are TM's that accept or reject without reading all their input.
-      We choose to eliminate them from consideration."\<close>
+      We choose to eliminate them from consideration.''\<close>
 
 (* TODO consider turning \<open>tcomp\<close> into a definition to avoid large proof terms *)
 abbreviation tcomp :: "(nat \<Rightarrow> 'a :: floor_ceiling) \<Rightarrow> nat \<Rightarrow> nat"
@@ -200,17 +200,17 @@ definition time :: "TM \<Rightarrow> tape \<Rightarrow> nat option"
     )"
 
 
-text\<open>Notion of time-constructible from Hopcroft ch. 12.3, p. 299:
-  "A function T(n) is said to be time constructible if there exists a T(n) time-
+text\<open>Notion of time-constructible from @{cite \<open>ch.~12.3\<close> hopcroftAutomata1979}:
+  ``A function T(n) is said to be time constructible if there exists a T(n) time-
   bounded multitape Turing machine M such that for each n there exists some input
-  on which M actually makes T(n) moves."\<close>
+  on which M actually makes T(n) moves.''\<close>
 
 definition tconstr :: "(nat \<Rightarrow> nat) \<Rightarrow> bool"
   where "tconstr T \<equiv> \<exists>M. \<forall>n. \<exists>w. time M ([], w) = Some (T n)"
 
-text\<open>Fully time-constructible, ibid.:
-  "We say that T(n) is fully time-constructible if there is a TM
-  that uses T(n) time on all inputs of length n."\<close>
+text\<open>Fully time-constructible, (@{cite \<open>ch.~12.3\<close> hopcroftAutomata1979}):
+  ``We say that T(n) is fully time-constructible if there is a TM
+  that uses T(n) time on all inputs of length n.''\<close>
 
 definition fully_tconstr :: "(nat \<Rightarrow> nat) \<Rightarrow> bool"
   where "fully_tconstr T \<equiv> \<exists>M. \<forall>n w. length w = n \<longrightarrow> time M ([], w) = Some (T n)"
@@ -288,7 +288,7 @@ lemma tape_size_mono: "n \<le> m \<Longrightarrow> space0 M x n \<le> space0 M x
 subsection\<open>Deciding Languages\<close>
 
 \<comment> \<open>Since \<open>L\<close> is a typical name for unspecified languages in the literature,
-    the synonymous constructor \<^term>\<open>Turing.action.L\<close> ("move head left") is hidden.\<close>
+    the synonymous constructor \<^term>\<open>Turing.action.L\<close> (\<^emph>\<open>move head left\<close>) is hidden.\<close>
 hide_const (open) L
 
 text\<open>A TM \<open>M\<close> is considered to halt on a word \<open>w\<close>, iff it reaches a final state upon input of \<open>w\<close>.
@@ -836,13 +836,13 @@ subsection\<open>Classical Results\<close>
 
 subsubsection\<open>Almost Everywhere\<close>
 
-text\<open>Hopcroft uses the finite control in Lemma 12.3
+text\<open>@{cite \<open>ch.~12.2\<close> hopcroftAutomata1979} uses the finite control in Lemma 12.3
   to make the jump from almost everywhere to everywhere:
 
-  "We say that a statement with parameter \<open>n\<close> is true \<^emph>\<open>almost everywhere\<close> (a.e.) if it
+  ``We say that a statement with parameter \<open>n\<close> is true \<^emph>\<open>almost everywhere\<close> (a.e.) if it
   is true for all but a finite number of values of \<open>n\<close>. We say a statement is true infinitely
   often (i.o.) if it is true for an infinite number of \<open>n\<close>'s. Note that both a statement and
-  its negation may be true i.o."\<close>
+  its negation may be true i.o.''\<close>
 
 definition almost_everywhere :: "('a \<Rightarrow> bool) \<Rightarrow> bool"
   where ae_def[simp]: "almost_everywhere P \<equiv> finite {x. \<not> P x}"
@@ -916,12 +916,14 @@ lemma ae_disj: "almost_everywhere P \<or> almost_everywhere Q \<Longrightarrow> 
   by auto
 
 
-text\<open>"Lemma 12.3  If \<open>L\<close> is accepted by a TM \<open>M\<close> that is \<open>S(n)\<close> space bounded a.e., then \<open>L\<close> is
+text\<open>From @{cite \<open>ch.~12.2\<close> hopcroftAutomata1979}:
+
+  ``\<^bold>\<open>Lemma 12.3\<close>  If \<open>L\<close> is accepted by a TM \<open>M\<close> that is \<open>S(n)\<close> space bounded a.e., then \<open>L\<close> is
   accepted by an \<open>S(n)\<close> space-bounded TM.
   Proof  Use the finite control to accept or reject strings of length \<open>n\<close> for the finite
   number of \<open>n\<close> where \<open>M\<close> is not \<open>S(n)\<close> bounded. Note that the construction is not
   effective, since in the absence of a time bound we cannot tell which of these words
-  \<open>M\<close> accepts."
+  \<open>M\<close> accepts.''
 
   The lemma is only stated for space bounds,
   but it seems reasonable that a similar construction works on time bounds.\<close>
@@ -977,20 +979,20 @@ lemma DTIME_mono_ae:
 
 subsubsection\<open>Linear Speed-Up\<close>
 
-text\<open>Hopcroft:
+text\<open>From @{cite \<open>ch.~12.2\<close> hopcroftAutomata1979}:
 
- "Theorem 12.3  If \<open>L\<close> is accepted by a \<open>k\<close>-tape \<open>T(n)\<close> time-bounded Turing machine
+ ``\<^bold>\<open>Theorem 12.3\<close>  If \<open>L\<close> is accepted by a \<open>k\<close>-tape \<open>T(n)\<close> time-bounded Turing machine
   \<open>M\<^sub>1\<close>, then \<open>L\<close> is accepted by a \<open>k\<close>-tape \<open>cT(n)\<close> time-bounded TM \<open>M\<^sub>2\<close> for any \<open>c > 0\<close>,
-  provided that \<open>k > 1\<close> and \<open>inf\<^sub>n\<^sub>\<rightarrow>\<^sub>\<infinity> T(n)/n = \<infinity>\<close>."
+  provided that \<open>k > 1\<close> and \<open>inf\<^sub>n\<^sub>\<rightarrow>\<^sub>\<infinity> T(n)/n = \<infinity>\<close>.''
 
   Note that the following formalization is likely false
-  (or at least not provable using the construction of Hopcroft),
-  as the used definition of TMs only allows single-tape TMs with a binary alphabet.\<close>
+  (or at least not provable using the construction from @{cite hopcroftAutomata1979}),
+  as the definition of TMs from @{cite xuIsabelleTM2013} only allows single-tape TMs with a binary alphabet.\<close>
 
 lemma linear_time_speed_up:
   assumes "c > 0"
-  \<comment> \<open>This assumption is stronger than the \<open>lim inf\<close> required by Hopcroft, but simpler to define in Isabelle.\<close>
-    and "\<forall>N. \<exists>n'. \<forall>n\<ge>n'. T(n)/n \<ge> N"
+    and "\<forall>N. \<exists>n'. \<forall>n\<ge>n'. T(n)/n \<ge> N" \<comment> \<open>This assumption is stronger than the \<open>lim inf\<close>
+      required by @{cite hopcroftAutomata1979}, but simpler to define in Isabelle.\<close>
     and "decides M\<^sub>1 L"
     and "time_bounded T M\<^sub>1"
   obtains M\<^sub>2 where "decides M\<^sub>2 L" and "time_bounded (\<lambda>n. c * T n) M\<^sub>2"
@@ -1107,15 +1109,15 @@ proof -
   define l where "l \<equiv> tape_size <w>\<^sub>t\<^sub>p"
 
     \<comment> \<open>Idea: We already know that the first machine \<^term>\<open>M\<^sub>R\<close> is time bounded
-    (@{thm \<open>time_bounded_word T\<^sub>R M\<^sub>R w\<close>}),
+    (see @{thm \<open>time_bounded_word T\<^sub>R M\<^sub>R w\<close>}),
     such that its run-time is bounded by \<^term>\<open>T\<^sub>R l = T\<^sub>R (2 * length w)\<close>
-    (@{thm tape_size_input}).
+    (note @{thm tape_size_input}).
 
     We also know that its execution will result in the encoded corresponding input word \<open>f\<^sub>R w\<close>
     (@{thm \<open>computes_word M\<^sub>R f\<^sub>R w\<close>}).
     Since the length of the corresponding input word is bounded by \<^term>\<open>l\<^sub>R (length w)\<close>
     (@{thm \<open>length (f\<^sub>R w) \<le> l\<^sub>R (length w)\<close>}),
-    and the second machine \<^term>\<open>M\<^sub>B\<close> is time bounded (@{thm \<open>time_bounded_word T\<^sub>B M\<^sub>B (f\<^sub>R w)\<close>}),
+    and the second machine \<^term>\<open>M\<^sub>B\<close> is time bounded (see @{thm \<open>time_bounded_word T\<^sub>B M\<^sub>B (f\<^sub>R w)\<close>}),
     we may conclude that the run-time of \<^term>\<open>M \<equiv> M\<^sub>R |+| M\<^sub>B\<close> on the input \<^term>\<open><w>\<^sub>t\<^sub>p\<close>
     is no longer than \<open>T l = T\<^sub>R (l\<^sub>R l) + T\<^sub>B l\<close>.
 
@@ -1149,9 +1151,7 @@ lemma reduce_DTIME':
     and f\<^sub>R :: "word \<Rightarrow> word" \<comment> \<open>the reduction\<close>
     and l\<^sub>R :: "nat \<Rightarrow> nat" \<comment> \<open>length bound of the reduction\<close>
   assumes f\<^sub>R_ae: "almost_everywhere (\<lambda>w. (f\<^sub>R w \<in> L\<^sub>1 \<longleftrightarrow> w \<in> L\<^sub>2) \<and> (length (f\<^sub>R w) \<le> l\<^sub>R (length w)))"
-    \<comment> \<open>The following assumption allows reasoning about \<^term>\<open>T\<close> and \<^term>\<open>l\<^sub>R\<close>,
-      as if both were \<^const>\<open>mono\<close>.\<close>
-    and "\<And>n. T (l\<^sub>R n) \<ge> T(n)"
+    and "\<And>n. T (l\<^sub>R n) \<ge> T(n)" \<comment> \<open>allows reasoning about \<^term>\<open>T\<close> and \<^term>\<open>l\<^sub>R\<close> as if both were \<^const>\<open>mono\<close>.\<close>
     and "computable_in_time T f\<^sub>R"
     and T_superlinear: "\<forall>N. \<exists>n. \<forall>m\<ge>n. T(m)/m \<ge> N"
     and "L\<^sub>1 \<in> DTIME(T)"

papers/116_stefan_rass/Goedel_Numbering.thy
@@ -7,14 +7,13 @@ begin
 type_synonym word = "bin"
 
 
-text\<open>Definition of Gödel numbers, given in ch. 3.1:
+text\<open>Definition of Gödel numbers, given in @{cite \<open>ch.~3.1\<close> rassOwf2017}:
 
-  "[A Gödel numbering] is a mapping \<open>gn : \<Sigma>\<^sup>* \<rightarrow> \<nat>\<close> that is computable, injective,
-  and such that \<open>gn(\<Sigma>\<^sup>*)\<close> is decidable and \<open>gn\<^sup>-\<^sup>1(n)\<close> is computable for all \<open>n \<in> \<nat>\<close> [10].
+ ``[A Gödel numbering] is a mapping \<open>gn : \<Sigma>\<^sup>* \<rightarrow> \<nat>\<close> that is computable, injective,
+  and such that \<open>gn(\<Sigma>\<^sup>*)\<close> is decidable and \<open>gn\<^sup>-\<^sup>1(n)\<close> is computable for all \<open>n \<in> \<nat>\<close> [...].
   The simple choice of \<open>gn(w) = (w)\<^sub>2\<close> is obviously not injective
   (since \<open>(0\<^sup>nw)\<^sub>2 = (w)\<^sub>2\<close> for all \<open>n \<in> \<nat>\<close> and all \<open>w \<in> \<Sigma>\<^sup>*\<close>), but this can be fixed
-  conditional on \<open>0 \<notin> \<nat>\<close> by setting
-                                \<open>gn(w) := (1w)\<^sub>2\<close>.                           (2)"\<close>
+  conditional on \<open>0 \<notin> \<nat>\<close> by setting \<open>gn(w) := (1w)\<^sub>2\<close>.''\<close>
 
 definition gn :: "word \<Rightarrow> nat" where "gn w = nat_of_bin (w @ [True])"
 
@@ -62,7 +61,7 @@ next
   then show "n \<in> range gn" by (intro image_eqI) blast+
 qed
 
-text\<open>[\<^const>\<open>gn\<close>] is a computable bijection between \<open>\<nat>\<close> and \<open>\<Sigma>\<^sup>*\<close>."\<close>
+text\<open>``[\<^const>\<open>gn\<close>] is a computable bijection between \<open>\<nat>\<close> and \<open>\<Sigma>\<^sup>*\<close>.''\<close>
 
 corollary gn_bij: "bij_betw gn UNIV {0<..}" using inj_gn range_gn by (intro bij_betw_imageI) blast+
 

papers/116_stefan_rass/L0.thy
@@ -52,7 +52,7 @@ locale tht_assms =
   fixes T t :: "nat \<Rightarrow> nat"
   assumes fully_tconstr_T: "fully_tconstr T"
 
-  \<comment> \<open>This assumption represents the statements containing \<open>lim\<close> (Rass) and \<open>lim inf\<close> (Hopcroft).
+  \<comment> \<open>This assumption represents the statements containing \<open>lim\<close> @{cite rassOwf2017} and \<open>lim inf\<close> @{cite hopcroftAutomata1979}.
       \<^const>\<open>LIMSEQ\<close> (\<^term>\<open>X \<longlonglongrightarrow> x\<close>) was chosen, as it seems to match the intended meaning
       (@{thm LIMSEQ_def lim_sequentially}).\<close>
     and T_dominates_t: "(\<lambda>n. t n * log 2 (t n) / T n) \<longlonglongrightarrow> 0"
@@ -61,11 +61,12 @@ locale tht_assms =
       Thus, the above assumption would trivially hold for \<open>T(n) = 0\<close>.\<close>
     and T_not_0: "T n \<noteq> 0"
 
-  \<comment> \<open>The following assumption is not found in the paper or the primary source (Hopcroft),
+  \<comment> \<open>The following assumption is not found in @{cite rassOwf2017} or the primary source @{cite hopcroftAutomata1979},
     but is taken from the AAU lecture slides of \<^emph>\<open>Algorithms and Complexity Theory\<close>.
+    \<^footnote>\<open>TODO properly cite this\<close>
     It patches a hole that allows one to prove \<^term>\<open>False\<close> from the Time Hierarchy Theorem below
     (\<open>time_hierarchy\<close>).
-    This is demonstrated in \<^file>\<open>../../isa_examples/THT_inconsistencies_MWE.thy\<close>.\<close>
+    This is demonstrated in \<^file>\<open>examples/THT_inconsistencies_MWE.thy\<close>.\<close>
     and t_min: "n \<le> t n"
 begin
 
@@ -119,8 +120,9 @@ qed
 
 text\<open>\<open>L\<^sub>D\<close>, defined as part of the proof for the Time Hierarchy Theorem.
 
-  "The “diagonal-language” \<open>L\<^sub>D\<close> is thus defined over the alphabet \<open>\<Sigma> = {0, 1}\<close> as
-       \<open>LD := {w \<in> \<Sigma>\<^sup>*: M\<^sub>w halts and rejects w within \<le> T(len(w)) steps}\<close>.    (9)"\<close>
+  ``The `diagonal-language' \<open>L\<^sub>D\<close> is thus defined over the alphabet \<open>\<Sigma> = {0, 1}\<close> as
+       \<open>LD := {w \<in> \<Sigma>\<^sup>*: M\<^sub>w halts and rejects w within \<le> T(len(w)) steps}\<close>.    (9)''
+  @{cite rassOwf2017}\<close>
 
 definition L\<^sub>D :: "lang"
   where LD_def[simp]: "L\<^sub>D \<equiv> {w. let M\<^sub>w = TM_decode_pad w in
@@ -228,12 +230,12 @@ end \<comment> \<open>\<^locale>\<open>tht_assms\<close>\<close>
 
 subsection\<open>The Hard Language \<open>L\<^sub>0\<close>\<close>
 
-text\<open>From the proof of Lemma 4.6, p14:
+text\<open>From the proof of Lemma 4.6@{cite rassOwf2017}:
 
-   "To retain \<open>L\<^sub>D \<inter> SQ \<in> DTIME(T)\<close>, we must choose \<open>T\<close> so large that the decision
+   ``To retain \<open>L\<^sub>D \<inter> SQ \<in> DTIME(T)\<close>, we must choose \<open>T\<close> so large that the decision
     \<open>w \<in> SQ\<close> is possible within the time limit incurred by \<open>T\<close>, so we add \<open>T(n) \<ge> n\<^sup>3\<close>
     to our hypothesis besides Assumption 4.4 (note that we do not need an optimal
-    complexity bound here)."\<close>
+    complexity bound here).''\<close>
 
 locale tht_sq_assms = tht_assms +
   assumes T_lower_bound: "T n \<ge> n^3"
@@ -324,7 +326,7 @@ next
 qed
 
 
-\<comment> \<open>For now assume that this result will hold for some version of \<open>L\<^sub>D\<close> and \<open>L\<^sub>0\<close>.\<close>
+text\<open>For now assume that this result will hold for some version of \<open>L\<^sub>D\<close> and \<open>L\<^sub>0\<close>.\<close>
 
 lemma L\<^sub>D_L\<^sub>0_adj_sq_iff:
   fixes w
@@ -334,8 +336,9 @@ lemma L\<^sub>D_L\<^sub>0_adj_sq_iff:
   sorry
 
 
-text\<open>Lemma 4.6. Let \<open>t\<close>, \<open>T\<close> be as in Assumption 4.4 and assume \<open>T(n) \<ge> n\<^sup>3\<close>.
-  Then, there exists a language \<open>L\<^sub>0 \<in> DTIME(T) - DTIME(t)\<close> for which \<open>dens\<^sub>L\<^sub>0(x) \<le> \<surd>x\<close>.\<close>
+text\<open>From @{cite rassOwf2017}:
+ ``Lemma 4.6. Let \<open>t\<close>, \<open>T\<close> be as in Assumption 4.4 and assume \<open>T(n) \<ge> n\<^sup>3\<close>.
+  Then, there exists a language \<open>L\<^sub>0 \<in> DTIME(T) - DTIME(t)\<close> for which \<open>dens\<^sub>L\<^sub>0(x) \<le> \<surd>x\<close>.''\<close>
 
 lemma L0_t: "L\<^sub>0 \<notin> DTIME(t)"
 proof (rule ccontr, unfold not_not)

papers/116_stefan_rass/LD_Issues.thy
@@ -7,7 +7,7 @@ begin
 
 subsection\<open>Encoding\<close>
 
-\<comment> \<open>Decode a pair of \<open>(l, x) \<in> \<nat> \<times> {0,1}*\<close> from its encoded form \<open>w \<in> {0,1}*\<close>.
+text\<open>Decode a pair of \<open>(l, x) \<in> \<nat> \<times> {0,1}*\<close> from its encoded form \<open>w \<in> {0,1}*\<close>.
 
   The encoding defined by this should have \<open>l\<close> encoded in the upper (more significant) bits of \<open>l\<parallel>x\<close>.
 
@@ -16,8 +16,8 @@ subsection\<open>Encoding\<close>
   If \<open>w\<close> does not match the expression, default values \<open>l = 0, x = []\<close> are assigned.
 
   Construction:
-  To ensure the required property for Lemma 4.6, the lower ~half of \<open>w\<close>
-  (the \<^term>\<open>suffix_len w\<close> least-significant-bits) is dropped.
+  To ensure the required property for Lemma 4.6@{cite rassOwf2017}, the lower half of \<open>w\<close>
+  (the \<open>k\<close> least-significant-bits) is dropped.
   Then, \<open>l\<close> is the number of leading \<open>1\<close>s.
   Remove all leading \<open>1\<close>s and one \<open>0\<close> to retain \<open>x\<close>.\<close>
 
@@ -96,9 +96,7 @@ subsection\<open>Definition\<close>
 context tht_assms
 begin
 
-\<comment> \<open>Alternative \<open>L\<^sub>D\<close>.
-
-  Note: this is not intended to replace \<^const>\<open>L\<^sub>D\<close>.
+text\<open>Note: this is not intended to replace \<^const>\<open>L\<^sub>D\<close>.
   Instead, the further proof uses the similarity of \<open>L\<^sub>D\<close> and \<open>L\<^sub>D''\<close>
   to prove properties of \<open>L\<^sub>D''\<close> via reduction to \<open>L\<^sub>D\<close>.
 
@@ -169,9 +167,7 @@ qed
 end \<comment> \<open>context \<^locale>\<open>tht_assms\<close>\<close>
 context tht_sq_assms begin
 
-
-\<comment> \<open>The following proof is similar to that of @{thm tht_sq_assms.L0_t}.\<close>
-lemma L\<^sub>D''_t: "L\<^sub>D'' \<notin> DTIME(t)"
+lemma L\<^sub>D''_t: "L\<^sub>D'' \<notin> DTIME(t)" \<comment> \<open>The proof is similar to that of @{thm tht_sq_assms.L0_t}.\<close>
 proof (rule ccontr, unfold not_not)
   assume assm: "L\<^sub>D'' \<in> DTIME(t)"
 
@@ -249,8 +245,8 @@ proof (rule reduce_DTIME)
 qed
 
 
-text\<open>Lemma 4.6. Let \<open>t\<close>, \<open>T\<close> be as in Assumption 4.4 and assume \<open>T(n) \<ge> n\<^sup>3\<close>.
-  Then, there exists a language \<open>L\<^sub>0 \<in> DTIME(T) - DTIME(t)\<close> for which \<open>dens\<^sub>L\<^sub>0(x) \<le> \<surd>x\<close>.\<close>
+text\<open>``\<^bold>\<open>Lemma 4.6.\<close> Let \<open>t\<close>, \<open>T\<close> be as in Assumption 4.4 and assume \<open>T(n) \<ge> n\<^sup>3\<close>.
+  Then, there exists a language \<open>L\<^sub>0 \<in> DTIME(T) - DTIME(t)\<close> for which \<open>dens\<^sub>L\<^sub>0(x) \<le> \<surd>x\<close>.''\<close>
 
 lemma L0''_t: "L\<^sub>0'' \<notin> DTIME(t)"
 proof (rule ccontr, unfold not_not)

commit 08acd1b1b41798b6375e1a278e4bc422e001a3dc

fix latex error
introduced in b2885a2cb12d2a5d843ca48ad3df2f0912a567f3

papers/116_stefan_rass/SQ.thy
@@ -172,7 +172,7 @@ qed (* case "x \<le> 0" by *) simp
 
 subsection\<open>Length of Prefix\<close>
 
-text\<open>\<open>w'\<close> (next_square n) will eventually have an identical lot of \<open>\<lceil>log l\<rceil>\<close> most significant bits.\<close>
+text\<open>\<open>w'\<close> (\<^term>\<open>next_square n\<close>) will eventually have an identical lot of \<open>\<lceil>log l\<rceil>\<close> most significant bits.\<close>
 
 (* TODO format the following note as text *)
 (*