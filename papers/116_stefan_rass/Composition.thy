CAVE-PNP/116-stefan-rass

commit e08c9086515f9f5350a0205acd9971092f9dae79

order of nth_or arguments

papers/116_stefan_rass/Composition.thy
@@ -11,7 +11,7 @@ subsubsection\<open>Reordering Tapes\<close>
 (* TODO consider renaming \<open>is\<close>. it stands for indices, but is a somewhat bad name since it is also an Isabelle keyword *)
 (* TODO consider extracting the function and general lemmas *)
 definition reorder :: "(nat option list) \<Rightarrow> 'a list \<Rightarrow> 'a list \<Rightarrow> 'a list"
-  where "reorder is xs ys = map2 (\<lambda>i x. case is ! i of None \<Rightarrow> x | Some i' \<Rightarrow> nth_or i' x ys) [0..<length xs] xs"
+  where "reorder is xs ys = map2 (\<lambda>i x. case is ! i of None \<Rightarrow> x | Some j \<Rightarrow> nth_or x j ys) [0..<length xs] xs"
 
 value "reorder [Some 0, Some 2, Some 1, None] [w,x,y,z] [a,b,c]"
 
@@ -22,15 +22,15 @@ lemma reorder_Nil[simp]: "reorder is xs [] = xs"
 
 lemma reorder_nth[simp]:
   assumes "i < length xs"
-  shows "reorder is xs ys ! i = (case is ! i of None \<Rightarrow> xs ! i | Some i' \<Rightarrow> nth_or i' (xs ! i) ys)"
+  shows "reorder is xs ys ! i = (case is ! i of None \<Rightarrow> xs ! i | Some j \<Rightarrow> nth_or (xs ! i) j ys)"
   using assms unfolding reorder_def by (subst nth_map2) auto
 
 lemma reorder_map[simp]: "map f (reorder is xs ys) = reorder is (map f xs) (map f ys)"
 proof -
-  let ?m = "\<lambda>d i x. case is ! i of None \<Rightarrow> d x | Some i' \<Rightarrow> nth_or i' (d x) (map f ys)"
+  let ?m = "\<lambda>d i x. case is ! i of None \<Rightarrow> d x | Some j \<Rightarrow> nth_or (d x) j (map f ys)"
   let ?m' = "\<lambda>d (i, x). ?m d i x" and ?id = "\<lambda>x. x"
   have "map f (reorder is xs ys) =
-    map (\<lambda>(i, x). f (case is ! i of None \<Rightarrow> x | Some i' \<Rightarrow> nth_or i' x ys)) (zip [0..<length xs] xs)"
+    map (\<lambda>(i, x). f (case is ! i of None \<Rightarrow> x | Some j \<Rightarrow> nth_or x j ys)) (zip [0..<length xs] xs)"
     unfolding reorder_def map_map comp_def by force
   also have "... = map2 (?m f) [0..<length xs] xs" unfolding option.case_distrib nth_or_map ..
   also have "... = map ((?m' ?id) \<circ> (\<lambda>(i, x). (i, f x))) (zip [0..<length xs] xs)" by fastforce

papers/116_stefan_rass/Supplementary/Lists.thy
@@ -300,16 +300,16 @@ qed
 text\<open>A version of \<^const>\<open>nth\<close> with a default value; if the requested element is not in the list,
   the default value is returned instead.\<close>
 
-definition nth_or :: "nat \<Rightarrow> 'a \<Rightarrow> 'a list \<Rightarrow> 'a"
-  where "nth_or n x xs \<equiv> if n < length xs then xs ! n else x"
+definition nth_or :: "'a \<Rightarrow> nat \<Rightarrow> 'a list \<Rightarrow> 'a"
+  where "nth_or x n xs \<equiv> if n < length xs then xs ! n else x"
 
 lemma nth_or_simps[simp]:
-  shows nth_or_Nil: "nth_or n x [] = x"
-    and nth_or_val: "n < length xs \<Longrightarrow> nth_or n x xs = xs ! n"
-    and nth_or_other: "\<not> n < length xs \<Longrightarrow> nth_or n x xs = x"
+  shows nth_or_Nil: "nth_or x n [] = x"
+    and nth_or_val: "n < length xs \<Longrightarrow> nth_or x n xs = xs ! n"
+    and nth_or_other: "\<not> n < length xs \<Longrightarrow> nth_or x n xs = x"
   unfolding nth_or_def by auto
 
-lemma nth_or_map: "f (nth_or n x xs) = nth_or n (f x) (map f xs)" by (cases "n < length xs") auto
+lemma nth_or_map: "f (nth_or x n xs) = nth_or (f x) n (map f xs)" by (cases "n < length xs") auto
 
 
 text\<open>Force a list to a given length; truncate if too long, and pad with the default value if too short.\<close>

papers/116_stefan_rass/TM.thy
@@ -475,10 +475,6 @@ subsubsection\<open>Steps\<close>
 context TM
 begin
 
-text\<open>If the current state is not final,
-  apply the action determined by \<^const>\<open>\<delta>\<close> for the current configuration.
-  Otherwise, do not execute any action.\<close>
-
 definition step_not_final :: "('q, 's) TM_config \<Rightarrow> ('q, 's) TM_config"
   where "step_not_final c = (let q=state c; hds=heads c in conf
       (next_state q hds)
@@ -506,7 +502,9 @@ proof (rule nth_equalityI, unfold length_map length_zip next_actions_length l l'
   finally show "map2 tape_action (\<delta>\<^sub>a q hds) tps ! i = tps' ! i" .
 qed (rule refl)
 
-
+text\<open>If the current state is not final,
+  apply the action determined by \<^const>\<open>\<delta>\<close> for the current configuration.
+  Otherwise, do not execute any action.\<close>
 definition step :: "('q, 's) TM_config \<Rightarrow> ('q, 's) TM_config"
   where "step c = (if state c \<in> F then c else step_not_final c)"
 
@@ -547,14 +545,13 @@ corollary final_mono': "mono (\<lambda>n. is_final (steps n c))"
   using final_mono by (intro monoI le_boolI)
 
 lemma final_steps_rev:
-  assumes "is_final (steps a c)"
-    and "is_final (steps b c)"
-  shows "steps a c = steps b c"
-proof (cases a b rule: le_cases)
+  assumes "is_final (steps n c)"
+    and "is_final (steps m c)"
+  shows "steps n c = steps m c"
+proof (cases n m rule: le_cases)
   case le with assms show ?thesis by (intro final_le_steps[symmetric]) next
   case ge with assms show ?thesis by (intro final_le_steps)
 qed
-
 
 paragraph\<open>Well-Formed Steps\<close>
 
@@ -573,7 +570,5 @@ lemma steps_valid_state: "state c \<in> Q \<Longrightarrow> state (steps n c) \<
 lemma wf_steps[intro]: "wf_config c \<Longrightarrow> wf_config (steps n c)" using wf_step by (elim funpow_induct)
 
 end \<comment> \<open>\<^locale>\<open>TM\<close>\<close>
-
-
 
 end

commit bd80e4598f2150d61da0a36c04daea151d427174

fix: issues introduced in e08c9086515f9f5350a0205acd9971092f9dae79

papers/116_stefan_rass/Composition.thy
@@ -227,10 +227,10 @@ next
         then have "i' \<in> {0..<k}" unfolding items_match[symmetric] in_these_eq .
         then have [simp]: "i' < k" by simp
 
-        then have nth_i': "nth_or i' x xs = xs ! i'" if "length xs = k" for x :: 'x and xs
+        then have nth_i': "nth_or x i' xs = xs ! i'" if "length xs = k" for x :: 'x and xs
           unfolding nth_or_def \<open>length xs = k\<close> by (rule if_P)
         from \<open>i < k'\<close> have "i < length tps'" unfolding l_tps' .
-        then have r_nth_or: "?rt tps ! i = nth_or i' (tps' ! i) tps" for tps
+        then have r_nth_or: "?rt tps ! i = nth_or (tps' ! i) i' tps" for tps
           by simp
 
         have \<delta>\<^sub>w': "M'.\<delta>\<^sub>w ?q ?hds' i = \<delta>\<^sub>w (state c) (heads c) i'"
@@ -433,7 +433,7 @@ proof (rule nth_equalityI, unfold reorder_length tape_offset_length)
     from \<open>a \<le> i\<close> have "\<not> i < a" by simp
 
     from \<open>a \<le> i\<close> and \<open>i < a + k\<close> have "a \<le> i \<and> i < a + k" by blast
-    then have "?lhs ! i = nth_or (i - a) (xs ! i) ys" unfolding * by simp
+    then have "?lhs ! i = nth_or (xs ! i) (i - a) ys" unfolding * by simp
     also from \<open>i - a < k\<close> have "... = ys ! (i - a)" unfolding nth_or_def \<open>length ys = k\<close> by simp
     also from \<open>i - a < k\<close> have "... = (ys @ take b (drop (a + k) xs)) ! (i - a)" by (subst nth_append) simp
     also from \<open>\<not> i < a\<close> have "... = ?rhs ! i" by (subst (2) nth_append) simp