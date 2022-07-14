eaed32c36a35e115aaa77b1971f0d4e72bfe0172
Max-Julian Jakobitsch <max-julian.jakobitsch@aau.at>
2022-07-14 18:46:42 +0200

    feat(Formal Languages): improve def
    change definition from set of words (that may contain symbols outside the alphabet) to a generating predicate.
    the `words` of the language L are now defined as the members of `(alphabet L)*` that fulfill the predicate.
    this makes `valid_lang` unnecessary

--- Formal_Languages.thy
+++ Formal_Languages.thy
@@ -2,28 +2,16 @@ theory Formal_Languages
   imports Main "Supplementary/Lists" Intro_Dest_Elim.IHOL_IDE
 begin
 
-datatype ('s) lang = Lang (alphabet: "'s set") (words: "'s list set")
+datatype ('s) lang = Lang (alphabet: "'s set") (gen_pred: "'s list \<Rightarrow> bool")
 
-definition valid_lang :: "'s lang \<Rightarrow> bool"
-  where "valid_lang L \<equiv> words L \<subseteq> (alphabet L)*"
-
-lemma valid_langI[intro]: "words L \<subseteq> (alphabet L)* \<Longrightarrow> valid_lang L"
-  unfolding valid_lang_def by blast
-lemma valid_langD[dest]: "valid_lang L \<Longrightarrow> w \<in> words L \<Longrightarrow> w \<in> (alphabet L)*"
-  unfolding valid_lang_def by blast
+definition words :: "'s lang \<Rightarrow> 's list set"
+  where "words L = {w\<in>(alphabet L)*. gen_pred L w}"
 
 
 definition member_lang :: "'s list \<Rightarrow> 's lang \<Rightarrow> bool" (infix "\<in>\<^sub>L" 50)
-  where "w \<in>\<^sub>L L \<equiv> w \<in> (alphabet L)* \<and> w \<in> words L"
+  where "w \<in>\<^sub>L L \<equiv> w \<in> words L"
 
 abbreviation not_member_lang :: "'s list \<Rightarrow> 's lang \<Rightarrow> bool" (infix "\<notin>\<^sub>L" 50)
   where "w \<notin>\<^sub>L L \<equiv> \<not> (w \<in>\<^sub>L L)"
-
-lemma valid_lang_member[simp, dest]: "valid_lang L \<Longrightarrow> w \<in>\<^sub>L L \<longleftrightarrow> w \<in> words L"
-  unfolding member_lang_def by blast
-
-
-lemma valid_lang_listsI[simp, intro]: "valid_lang (Lang \<Sigma> {w\<in>\<Sigma>*. P})"
-  unfolding valid_lang_def by auto
 
 end
