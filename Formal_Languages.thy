222941c302f166ebb00da68f31df3b14e0323738
Max-Julian Jakobitsch <max-julian.jakobitsch@aau.at>
2022-07-22 14:16:09 +0200

    feat: patch language density
    for now defined for `bool lang`, subject to change if gn is reworked

--- Formal_Languages.thy
+++ Formal_Languages.thy
@@ -18,4 +18,25 @@ lemma member_lang_iff[iff]: "w \<in>\<^sub>L L \<longleftrightarrow> w\<in>(alph
   unfolding words_def by blast
 
 
+text\<open>Defining complement and intersection analogous to sets.
+  See \<^const>\<open>inter\<close> and @{thm uminus_set_def inf_set_def}.\<close>
+
+instantiation lang :: (type) uminus begin
+definition "- L \<equiv> Lang (alphabet L) (- gen_pred L)" instance .. end
+
+instantiation lang :: (type) inf begin
+definition "inf_lang A B \<equiv> Lang (alphabet A \<inter> alphabet B) (\<lambda>w. gen_pred A w \<and> gen_pred B w)"
+instance .. end
+
+abbreviation inter_lang :: "'s lang \<Rightarrow> 's lang \<Rightarrow> 's lang" (infixl "\<inter>\<^sub>L" 70)
+  where "inter_lang \<equiv> inf"
+
+lemma inf_lang_altdef: "Lang \<Sigma>1 P1 \<inter>\<^sub>L Lang \<Sigma>2 P2 = Lang (inf \<Sigma>1 \<Sigma>2) (inf P1 P2)"
+  unfolding inf_lang_def by auto
+
+lemma inter_lang_commute: "L\<^sub>1 \<inter>\<^sub>L L\<^sub>2 = L\<^sub>2 \<inter>\<^sub>L L\<^sub>1" unfolding inf_lang_def by blast
+
+lemma inter_lang_words[simp]: "words (L\<^sub>1 \<inter>\<^sub>L L\<^sub>2) = words L\<^sub>1 \<inter> words L\<^sub>2"
+  unfolding inf_lang_def lang.sel words_def by (induction L\<^sub>1, induction L\<^sub>2) auto
+
 end
