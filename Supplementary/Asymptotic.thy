4429f7c1b8cd2c058de5177fa8320f3f0aa1aa2a
Max-Julian Jakobitsch <max-julian.jakobitsch@aau.at>
2022-07-28 14:44:27 +0200

    refactor: use syntax ∀⇩∞ instead of MOST, rename lemmas MOST => ae

--- Supplementary/Asymptotic.thy
+++ Supplementary/Asymptotic.thy
@@ -62,9 +62,9 @@ lemma suff_large_iff:
 
 text\<open>Relationship of \<^emph>\<open>sufficiently large\<close> and \<^emph>\<open>almost everywhere\<close>\<close>
 
-lemma MOST_suff_large:
+lemma ae_suff_large:
   fixes P :: "'a::{linorder, no_top} \<Rightarrow> bool"
-  assumes "MOST n. P n"
+  assumes "\<forall>\<^sub>\<infinity>n. P n"
   shows "\<exists>n\<^sub>0. \<forall>n\<ge>n\<^sub>0. P n"
 proof -
   from assms have "P n" if "n > Max {n. \<not> P n}" for n
@@ -73,37 +73,37 @@ proof -
   then show "\<exists>n\<^sub>0. \<forall>n\<ge>n\<^sub>0. P n" by (blast intro: suff_large_gt_imp_ge)
 qed
 
-lemma suff_large_MOST:
+lemma suff_large_ae:
   fixes P :: "nat \<Rightarrow> bool"
   assumes "\<exists>n\<^sub>0. \<forall>n\<ge>n\<^sub>0. P n"
-  shows "MOST n. P n"
+  shows "\<forall>\<^sub>\<infinity>n. P n"
 proof -
   from assms obtain n\<^sub>0 where *: "\<forall>n\<ge>n\<^sub>0. P n" by blast
   then have "{n. \<not> P n} \<subseteq> {n. n < n\<^sub>0}" using linorder_le_less_linear by blast
-  then show "MOST n. P n" unfolding eventually_cofinite
+  then show "\<forall>\<^sub>\<infinity>n. P n" unfolding eventually_cofinite
     using finite_Collect_less_nat rev_finite_subset by blast
 qed
 
-lemma MOST_suff_large_iff[iff]:
+lemma ae_suff_large_iff[iff]:
   fixes P :: "nat \<Rightarrow> bool"
-  shows "(MOST x. P x) \<longleftrightarrow> (\<exists>n\<^sub>0. \<forall>n\<ge>n\<^sub>0. P n)"
-  using MOST_suff_large and suff_large_MOST by (rule iffI)
+  shows "(\<forall>\<^sub>\<infinity>n. P n) \<longleftrightarrow> (\<exists>n\<^sub>0. \<forall>n\<ge>n\<^sub>0. P n)"
+  using ae_suff_large and suff_large_ae by (rule iffI)
 
-lemma MOST_suff_largeI:
+lemma ae_suff_largeI:
   fixes P :: "nat \<Rightarrow> bool"
   assumes "\<And>n. n\<ge>n\<^sub>0 \<Longrightarrow> P n"
-  shows "MOST n. P n"
-  unfolding MOST_suff_large_iff using assms by blast
+  shows "\<forall>\<^sub>\<infinity>n. P n"
+  unfolding ae_suff_large_iff using assms by blast
 
 
-method MOST_intro uses add =
+method ae_intro uses add =
   insert add,
-  (fold MOST_suff_large_iff)?,
+  (fold ae_suff_large_iff)?,
   (elim eventually_rev_mp)?,
-  intro MOST_suff_largeI impI
+  intro ae_suff_largeI impI
 
-text\<open>To solve goals like \<open>\<lbrakk>MOST n. A n; MOST n. B n; ...\<rbrakk> \<Longrightarrow> MOST n. P n\<close>, where \<open>n :: nat\<close>,
-  apply @{method MOST_intro}, then prove the resulting goal
+text\<open>To solve goals like \<open>\<lbrakk>\<forall>\<^sub>\<infinity>n. A n; \<forall>\<^sub>\<infinity>n. B n; ...\<rbrakk> \<Longrightarrow> \<forall>\<^sub>\<infinity>n. P n\<close>, where \<open>n :: nat\<close>,
+  apply @{method ae_intro}, then prove the resulting goal
   of the form \<open>\<And>n. \<lbrakk>n\<ge>n\<^sub>0; A n; B n; ...\<rbrakk> \<Longrightarrow> P n\<close>.
   The additional premise \<open>n\<ge>n\<^sub>0\<close> allows specifying an arbitrary minimum,
   as many lemmas require proving \<open>n>0\<close> or similar.\<close>
@@ -111,7 +111,7 @@ text\<open>To solve goals like \<open>\<lbrakk>MOST n. A n; MOST n. B n; ...\<rb
 
 subsection\<open>Asymptotic Domination\<close>
 
-lemma dominates_MOST_helper:
+lemma dominates_ae_helper:
   fixes c :: real
   assumes "c > 0"
     and "T n \<noteq> 0"
@@ -125,32 +125,32 @@ proof -
   finally show ?thesis .
 qed
 
-lemma dominates_MOST:
+lemma dominates_ae:
   fixes T t :: "nat \<Rightarrow> real" and c :: real
   assumes "(\<lambda>n. t n / T n) \<longlonglongrightarrow> 0"
-    and "MOST n. T n \<noteq> 0"
+    and "\<forall>\<^sub>\<infinity>n. T n \<noteq> 0"
     and "c > 0"
-  shows "MOST n. c * \<bar>t n\<bar> < \<bar>T n\<bar>"
+  shows "\<forall>\<^sub>\<infinity>n. c * \<bar>t n\<bar> < \<bar>T n\<bar>"
 proof -
   let ?r = "1/c"
   from \<open>c > 0\<close> have "?r > 0" by simp
-  with \<open>(\<lambda>n. t n / T n) \<longlonglongrightarrow> 0\<close> have "MOST n. \<bar>t n / T n\<bar> < ?r"
+  with \<open>(\<lambda>n. t n / T n) \<longlonglongrightarrow> 0\<close> have "\<forall>\<^sub>\<infinity>n. \<bar>t n / T n\<bar> < ?r"
     unfolding lim_sequentially dist_real_def diff_0_right by blast
-  with \<open>MOST n. T n \<noteq> 0\<close> show "MOST n. c * \<bar>t n\<bar> < \<bar>T n\<bar>"
-  proof (MOST_intro)
+  with \<open>\<forall>\<^sub>\<infinity>n. T n \<noteq> 0\<close> show "\<forall>\<^sub>\<infinity>n. c * \<bar>t n\<bar> < \<bar>T n\<bar>"
+  proof (ae_intro)
     fix n
     assume "\<bar>t n / T n\<bar> < ?r" "T n \<noteq> 0"
-    with \<open>c > 0\<close> show"c * \<bar>t n\<bar> < \<bar>T n\<bar>" by (subst dominates_MOST_helper)
+    with \<open>c > 0\<close> show"c * \<bar>t n\<bar> < \<bar>T n\<bar>" by (subst dominates_ae_helper)
   qed
 qed
 
-lemma MOST_dominates:
+lemma ae_dominates:
   fixes T t :: "nat \<Rightarrow> real" and c :: real
-  assumes "\<And>c. MOST n. c * \<bar>t n\<bar> < \<bar>T n\<bar>"
+  assumes "\<And>c. \<forall>\<^sub>\<infinity>n. c * \<bar>t n\<bar> < \<bar>T n\<bar>"
   shows "(\<lambda>n. t n / T n) \<longlonglongrightarrow> 0"
 proof -
-  have "MOST n. \<bar>t n / T n\<bar> < r" if "r > 0" for r
-  proof (MOST_intro add: \<open>MOST n. (1/r) * \<bar>t n\<bar> < \<bar>T n\<bar>\<close>)
+  have "\<forall>\<^sub>\<infinity>n. \<bar>t n / T n\<bar> < r" if "r > 0" for r
+  proof (ae_intro add: \<open>\<forall>\<^sub>\<infinity>n. (1/r) * \<bar>t n\<bar> < \<bar>T n\<bar>\<close>)
     from \<open>r > 0\<close> have "1/r > 0" by simp
     fix n
     assume "(1/r) * \<bar>t n\<bar> < \<bar>T n\<bar>"
@@ -161,7 +161,7 @@ proof -
     next
       assume "T n \<noteq> 0"
       with \<open>(1/r) * \<bar>t n\<bar> < \<bar>T n\<bar>\<close> and \<open>1/r > 0\<close> have "\<bar>t n / T n\<bar> < 1/(1/r)"
-        by (subst (asm) dominates_MOST_helper)
+        by (subst (asm) dominates_ae_helper)
       also have "... = r" by simp
       finally show ?thesis .
     qed
@@ -172,24 +172,24 @@ qed
 
 lemma dominates_altdef:
   fixes T t :: "nat \<Rightarrow> real"
-  assumes "MOST n. T n \<noteq> 0"
-  shows "((\<lambda>n. t n / T n) \<longlonglongrightarrow> 0) \<longleftrightarrow> (\<forall>c>0. MOST n. c * \<bar>t n\<bar> < \<bar>T n\<bar>)"
+  assumes "\<forall>\<^sub>\<infinity>n. T n \<noteq> 0"
+  shows "((\<lambda>n. t n / T n) \<longlonglongrightarrow> 0) \<longleftrightarrow> (\<forall>c>0. \<forall>\<^sub>\<infinity>n. c * \<bar>t n\<bar> < \<bar>T n\<bar>)"
 proof
   assume "(\<lambda>n. t n / T n) \<longlonglongrightarrow> 0"
-  then show "\<forall>c>0. MOST n. c * \<bar>t n\<bar> < \<bar>T n\<bar>" using assms by (intro allI impI dominates_MOST)
+  then show "\<forall>c>0. \<forall>\<^sub>\<infinity>n. c * \<bar>t n\<bar> < \<bar>T n\<bar>" using assms by (intro allI impI dominates_ae)
 next
-  assume asm: "\<forall>c>0. MOST n. c * \<bar>t n\<bar> < \<bar>T n\<bar>"
+  assume asm: "\<forall>c>0. \<forall>\<^sub>\<infinity>n. c * \<bar>t n\<bar> < \<bar>T n\<bar>"
   show "(\<lambda>n. t n / T n) \<longlonglongrightarrow> 0"
-  proof (intro MOST_dominates)
+  proof (intro ae_dominates)
     fix c
-    show "MOST n. c * \<bar>t n\<bar> < \<bar>T n\<bar>"
+    show "\<forall>\<^sub>\<infinity>n. c * \<bar>t n\<bar> < \<bar>T n\<bar>"
     proof (cases "c > 0")
       assume "c > 0"
       with asm show ?thesis by blast
     next
       assume "\<not> c > 0" hence "c \<le> 0" by simp
-      from \<open>MOST n. T n \<noteq> 0\<close> show ?thesis
-      proof (MOST_intro)
+      from \<open>\<forall>\<^sub>\<infinity>n. T n \<noteq> 0\<close> show ?thesis
+      proof (ae_intro)
         fix n assume "T n \<noteq> 0"
         from \<open>c \<le> 0\<close> have "c * \<bar>t n\<bar> \<le> 0" by (intro mult_nonneg_nonpos2) auto
         also from \<open>T n \<noteq> 0\<close> have "0 < \<bar>T n\<bar>" by simp
@@ -202,19 +202,19 @@ qed
 lemma dominates_mono:
   fixes T t :: "nat \<Rightarrow> real"
   assumes "(\<lambda>n. f n / T n) \<longlonglongrightarrow> 0"
-    and "MOST n. T n \<noteq> 0"
-    and "MOST n. \<bar>g n\<bar> \<le> \<bar>f n\<bar>"
+    and "\<forall>\<^sub>\<infinity>n. T n \<noteq> 0"
+    and "\<forall>\<^sub>\<infinity>n. \<bar>g n\<bar> \<le> \<bar>f n\<bar>"
   shows "(\<lambda>n. g n / T n) \<longlonglongrightarrow> 0"
 proof -
-  note * = dominates_altdef[OF \<open>MOST n. T n \<noteq> 0\<close>]
+  note * = dominates_altdef[OF \<open>\<forall>\<^sub>\<infinity>n. T n \<noteq> 0\<close>]
 
   show "(\<lambda>n. g n / T n) \<longlonglongrightarrow> 0" unfolding *
   proof (intro allI impI)
     fix c :: real
     assume "c > 0"
-    with \<open>(\<lambda>n. f n / T n) \<longlonglongrightarrow> 0\<close> have "MOST n. c * \<bar>f n\<bar> < \<bar>T n\<bar>" unfolding * by blast
-    with \<open>MOST n. \<bar>g n\<bar> \<le> \<bar>f n\<bar>\<close> show "MOST n. c * \<bar>g n\<bar> < \<bar>T n\<bar>"
-    proof (MOST_intro)
+    with \<open>(\<lambda>n. f n / T n) \<longlonglongrightarrow> 0\<close> have "\<forall>\<^sub>\<infinity>n. c * \<bar>f n\<bar> < \<bar>T n\<bar>" unfolding * by blast
+    with \<open>\<forall>\<^sub>\<infinity>n. \<bar>g n\<bar> \<le> \<bar>f n\<bar>\<close> show "\<forall>\<^sub>\<infinity>n. c * \<bar>g n\<bar> < \<bar>T n\<bar>"
+    proof (ae_intro)
       fix n :: nat
       assume "\<bar>g n\<bar> \<le> \<bar>f n\<bar>"
       moreover from \<open>c > 0\<close> have "c \<ge> 0" by simp
